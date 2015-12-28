8c7f2c38361d6a791cae8688b80ddac4affe8d13
gan <1078429355@qq.com>
2015-12-28 15:07:43 +0800

    修改快速排序

--- src/com/code/sort/Sort.java
+++ src/com/code/sort/Sort.java
@@ -41,7 +41,7 @@ public class Sort {
     public static void quickSort(int s[], int left, int right) {
         if (left < right) {//递归结束的条件，left==right
             int i = left, j = right;//定义左右两个指针
-            int key = s[left];//挖第一个坑
+            int key = s[i];//挖第一个坑
             while (i < j) {
                 while (s[j] >= key && i < j) {//从右往左找第一个小于等于key的数
                     j--;
